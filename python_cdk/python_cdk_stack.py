98b7862f98b1a64d9c0f81c1fc8b79f689364cb5
Jay Merrifield <jay.merrifield@devtechnology.com>
2023-01-24 17:23:25 -0500

    lambda_demo

--- python_cdk/python_cdk_stack.py
+++ python_cdk/python_cdk_stack.py
@@ -1,7 +1,12 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_s3_notifications as s3n,
+    aws_lambda_event_sources as lambda_event_sources,
+    aws_lambda as lambda_,
+    aws_lambda_python_alpha as python_lambda_
 )
 from constructs import Construct
 
@@ -11,9 +16,33 @@ class PythonCdkStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        my_lambda = lambda_.Function(
+            self, 'HelloHandler',
+            runtime=lambda_.Runtime.PYTHON_3_8,
+            code=lambda_.Code.from_asset('lambda'),
+            handler='index.handler',
+        )
+
+        # my_lambda = python_lambda_.PythonFunction(self, "HelloHandler2",
+        #     entry="lambda",
+        #     runtime=lambda_.Runtime.PYTHON_3_8,
+        # )
 
         # example resource
         # queue = sqs.Queue(
         #     self, "PythonCdkQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+
+        # sqs_event_source = lambda_event_sources.SqsEventSource(queue)
+        # my_lambda.add_event_source(sqs_event_source)
+
+        # bucket = s3.Bucket(self, "MyEncryptedBucket",
+        #     encryption=s3.BucketEncryption.KMS
+        # )
+        # bucket.add_event_notification(s3.EventType.OBJECT_CREATED, s3n.SqsDestination(queue))
+
+        # bucket_event_source = lambda_event_sources.S3EventSource(bucket,
+        #     events=[s3.EventType.OBJECT_CREATED, s3.EventType.OBJECT_REMOVED],
+        # )
+        # my_lambda.add_event_source(bucket_event_source)
